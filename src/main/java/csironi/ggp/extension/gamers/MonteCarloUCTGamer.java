0ff95509cd6a06fe0b534d134bc4ef3ba33d3743
Chiara <sironi.chiara.f@gmail.com>
2015-04-02 17:37:36 +0200

    Added MonteCarloUCTGamer class, not yet implementing UCT MCTS as action selection method, but just random selection.

---
+++ src/main/java/csironi/ggp/extension/gamers/MonteCarloUCTGamer.java
@@ -0,0 +1,76 @@
+package csironi.ggp.extension.gamers;
+
+import java.util.List;
+import java.util.Random;
+
+import org.ggp.base.player.gamer.event.GamerSelectedMoveEvent;
+import org.ggp.base.player.gamer.exception.GamePreviewException;
+import org.ggp.base.player.gamer.statemachine.StateMachineGamer;
+import org.ggp.base.util.game.Game;
+import org.ggp.base.util.statemachine.Move;
+import org.ggp.base.util.statemachine.StateMachine;
+import org.ggp.base.util.statemachine.exceptions.GoalDefinitionException;
+import org.ggp.base.util.statemachine.exceptions.MoveDefinitionException;
+import org.ggp.base.util.statemachine.exceptions.TransitionDefinitionException;
+import org.ggp.base.util.statemachine.implementation.prover.ProverStateMachine;
+
+public final class MonteCarloUCTGamer extends StateMachineGamer {
+
+	public MonteCarloUCTGamer() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public StateMachine getInitialStateMachine() {
+		return new ProverStateMachine();
+	}
+
+	@Override
+	public void stateMachineMetaGame(long timeout)
+			throws TransitionDefinitionException, MoveDefinitionException,
+			GoalDefinitionException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Move stateMachineSelectMove(long timeout)
+			throws TransitionDefinitionException, MoveDefinitionException,
+			GoalDefinitionException {
+		long start = System.currentTimeMillis();
+
+		List<Move> moves = getStateMachine().getLegalMoves(getCurrentState(), getRole());
+		Move selection = (moves.get(new Random().nextInt(moves.size())));
+
+		long stop = System.currentTimeMillis();
+
+		notifyObservers(new GamerSelectedMoveEvent(moves, selection, stop - start));
+		return selection;
+	}
+
+	@Override
+	public void stateMachineStop() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void stateMachineAbort() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void preview(Game g, long timeout) throws GamePreviewException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getName() {
+
+		return "MCTS UCT Gamer";
+
+	}
+
+}
