dc59a56d2d3cdbabd37c5afe9b47dd0a80420b26
Chiara <sironi.chiara.f@gmail.com>
2015-09-16 15:32:55 +0200

    Finished implementing the StateMachineSpeedTest class that tests the speed (iterations/second, nodes/second) of a state machine running Monte Carlo playouts. Also finished implementing the PropnetSpeedTest class that prepares the propnet state machine and runs the speed test for every game in the GGP Games repository.

--- src/main/java/csironi/ggp/course/PropnetSpeedTest.java
+++ src/main/java/csironi/ggp/course/PropnetSpeedTest.java
@@ -9,10 +9,9 @@ import org.ggp.base.util.game.GameRepository;
 import org.ggp.base.util.gdl.grammar.Gdl;
 import org.ggp.base.util.logging.GamerLogger;
 import org.ggp.base.util.logging.GamerLogger.FORMAT;
+import org.ggp.base.util.match.Match;
 import org.ggp.base.util.statemachine.exceptions.PropnetCreationException;
 import org.ggp.base.util.statemachine.implementation.propnet.ForwardInterruptingPropNetStateMachine;
-import org.ggp.base.util.statemachine.implementation.prover.ProverStateMachine;
-import org.ggp.base.util.statemachine.verifier.StateMachineVerifier;
 
 /**
  * @author C.Sironi
@@ -25,11 +24,13 @@ public class PropnetSpeedTest {
 	 */
 	public static void main(String[] args) {
 		long buildingTime = 300000L;
-		long testTime = 10000L;
+		long testTime = 60000L;
+
+		String gameToTest = null;
 
 		if (args.length != 0){
 
-			if(args.length == 2){
+			if(args.length >= 2){
 				try{
 					buildingTime = Long.parseLong(args[0]);
 					testTime = Long.parseLong(args[1]);
@@ -37,7 +38,10 @@ public class PropnetSpeedTest {
 				}catch(NumberFormatException nfe){
 					System.out.println("Inconsistent time values specification! Running tests with default settings:");
 					buildingTime = 300000L;
-					testTime = 10000L;
+					testTime = 60000L;
+				}
+				if(args.length > 2){
+					gameToTest = args[2];
 				}
 			}else{
 				System.out.println("Inconsistent time values specification! Running tests with default settings:");
@@ -50,58 +54,75 @@ public class PropnetSpeedTest {
 		System.out.println("Running time for each test: " + testTime);
 		System.out.println();
 
+		if(gameToTest != null){
+			System.out.println("Running test only on game " +  gameToTest);
+		}else{
+			System.out.println("Running tests on ALL games.");
+		}
+
+		System.out.println();
+
         ForwardInterruptingPropNetStateMachine thePropNetMachine;
 
-        GamerLogger.setSpilloverLogfile("PropnetVerifierTable.csv");
-        GamerLogger.log(FORMAT.CSV_FORMAT, "PropnetVerifierTable", "Game key;Construction Time (ms);Rounds;Test duration (ms);Pass;");
+        GamerLogger.setSpilloverLogfile("PropnetSpeedTestTable.csv");
+        GamerLogger.log(FORMAT.CSV_FORMAT, "PropnetSpeedTestTable", "Game key;Construction Time (ms);Test Duration (ms);Succeeded Iterations;Failed Iterations;Visited Nodes;Iterations/second;Nodes/second;");
 
         GameRepository theRepository = GameRepository.getDefaultRepository();
         for(String gameKey : theRepository.getGameKeys()) {
             if(gameKey.contains("laikLee")) continue;
 
+            if(gameToTest != null && !gameKey.equals(gameToTest)) continue;
+
             //if(!gameKey.equals("3pConnectFour") && !gameKey.equals("god")) continue;
 
             //if(!gameKey.equals("mummymaze1p")) continue;
 
-            GamerLogger.setSpilloverLogfile("PropnetVerifierLogs.log");
+            Match fakeMatch = new Match(gameKey + "." + System.currentTimeMillis(), -1, -1, -1,theRepository.getGame(gameKey) );
 
-            GamerLogger.log("StateMachine", "Testing on game " + gameKey);
+            GamerLogger.startFileLogging(fakeMatch, "PropnetSpeedTester");
+
+            GamerLogger.log("SMSpeedTest", "Testing on game " + gameKey);
 
             List<Gdl> description = theRepository.getGame(gameKey).getRules();
 
-
-            theReference = new ProverStateMachine();
-
-            // Create propnet state machine giving it 5 minutes to build the propnet
+            // Create propnet state machine giving it buildingTime milliseconds to build the propnet
             thePropNetMachine = new ForwardInterruptingPropNetStateMachine(buildingTime);
 
-            theReference.initialize(description);
-
-            boolean pass = false;
-            int rounds = -1;
-            long duration = 0L;
+            long testDuration = -1L;
+            int succeededIterations = -1;
+            int failedIterations = -1;
+            int visitedNodes = -1;
+            double iterationsPerSecond = -1;
+            double nodesPerSecond = -1;
 
             // Try to initialize the propnet state machine.
             // If initialization fails, skip the test.
             try{
+            	System.out.println("Detected activation in game " + gameKey + ".");
             	thePropNetMachine.initialize(description);
-            	System.out.println("Detected activation in game " + gameKey + ". Checking consistency: ");
-            	long start = System.currentTimeMillis();
-                pass = StateMachineVerifier.checkMachineConsistency(theReference, thePropNetMachine, testTime);
-                duration = System.currentTimeMillis() - start;
-                rounds = StateMachineVerifier.lastRounds;
+            	System.out.println("Propnet creation succeeded. Checking speed.");
+            	StateMachineSpeedTest.testSpeed(thePropNetMachine, testTime);
+
+            	testDuration = StateMachineSpeedTest.exactTimeSpent;
+            	succeededIterations = StateMachineSpeedTest.succeededIterations;
+         		failedIterations = StateMachineSpeedTest.failedIterations;
+                visitedNodes = StateMachineSpeedTest.visitedNodes;
+                iterationsPerSecond = ((double) succeededIterations * 1000)/((double) testDuration);
+                nodesPerSecond = ((double) visitedNodes * 1000)/((double) testDuration);
             }catch(PropnetCreationException re){
-            	GamerLogger.log("StateMachine", "No propnet available. Impossible to test this game. Cause: " + re.getMessage());
+            	GamerLogger.log("SMSpeedTest", "No propnet available. Impossible to test its speed in this game. Cause: " + re.getMessage());
             	System.out.println("Skipping test on game " + gameKey + ". No propnet available.");
             }
 
-            GamerLogger.log(FORMAT.PLAIN_FORMAT, "StateMachine", "");
+            GamerLogger.log(FORMAT.PLAIN_FORMAT, "SMSpeedTest", "");
 
-            GamerLogger.setSpilloverLogfile("PropnetVerifierTable.csv");
-            GamerLogger.log(FORMAT.CSV_FORMAT, "PropnetVerifierTable", gameKey + ";" + thePropNetMachine.getConstructionTime() + ";" + rounds + ";" + duration + ";" + pass + ";");
+            GamerLogger.stopFileLogging();
+            GamerLogger.log(FORMAT.CSV_FORMAT, "PropnetSpeedTestTable", gameKey + ";" + thePropNetMachine.getConstructionTime() + ";" + testDuration + ";" + succeededIterations + ";" + failedIterations + ";" + visitedNodes + ";" + iterationsPerSecond + ";" + nodesPerSecond + ";");
+
         }
-	}
 
 	}
+
+
 
 }
--- src/main/java/csironi/ggp/course/StateMachineSpeedTest.java
+++ src/main/java/csironi/ggp/course/StateMachineSpeedTest.java
@@ -1,69 +1,47 @@
 package csironi.ggp.course;
 
-import java.util.List;
-
-import org.ggp.base.player.gamer.event.GamerSelectedMoveEvent;
 import org.ggp.base.util.logging.GamerLogger;
-import org.ggp.base.util.statemachine.Move;
+import org.ggp.base.util.statemachine.MachineState;
+import org.ggp.base.util.statemachine.StateMachine;
+import org.ggp.base.util.statemachine.exceptions.MoveDefinitionException;
+import org.ggp.base.util.statemachine.exceptions.TransitionDefinitionException;
 
 public class StateMachineSpeedTest {
 
-	private int visitedNodes;
-	private int iterations;
+	public static int visitedNodes;
+	public static int succeededIterations;
+	public static int failedIterations;
+	public static long exactTimeSpent;
 
 
-	public void testSpeed(StateMachine theMachine){
+	public static void testSpeed(StateMachine theMachine, long timeToSpend){
 
-		this.visitedNodes = 0;
-		this.iterations = 0;
+		visitedNodes = 0;
+		succeededIterations = 0;
+		failedIterations = 0;
+		exactTimeSpent = 0L;
 
+		int[] lastIterationVisitedNodes = new int[1];
 
-		List<Move> moves = theMachine.getLegalMoves(getCurrentState(), getRole(), long timeToSpend);
+		MachineState initialState = theMachine.getInitialState();
 
-		Move selection = moves.get(0);
-		if (moves.size() > 1) {
-    		int[] moveTotalPoints = new int[moves.size()];
-    		int[] moveTotalAttempts = new int[moves.size()];
+		long startTime = System.currentTimeMillis();
 
-    		// Perform depth charges for each candidate move, and keep track
-    		// of the total score and total attempts accumulated for each move.
-    		for (int i = 0; true; i = (i+1) % moves.size()) {
-    		    if (System.currentTimeMillis() > finishBy)
-    		        break;
+		while(System.currentTimeMillis() < startTime + timeToSpend){
 
-    		    int theScore = performDepthChargeFromMove(getCurrentState(), moves.get(i));
-    		    moveTotalPoints[i] += theScore;
-    		    moveTotalAttempts[i] += 1;
-    		    visitedNodes += this.depth[0] + 1;
-    		    iterations++;
-
-    		}
-
-    		// Compute the expected score for each move.
-    		double[] moveExpectedPoints = new double[moves.size()];
-    		for (int i = 0; i < moves.size(); i++) {
-    		    moveExpectedPoints[i] = (double)moveTotalPoints[i] / moveTotalAttempts[i];
-    		}
-
-    		// Find the move with the best expected score.
-    		int bestMove = 0;
-    		double bestMoveScore = moveExpectedPoints[0];
-    		for (int i = 1; i < moves.size(); i++) {
-    		    if (moveExpectedPoints[i] > bestMoveScore) {
-    		        bestMoveScore = moveExpectedPoints[i];
-    		        bestMove = i;
-    		    }
-    		}
-    		selection = moves.get(bestMove);
+			try {
+				theMachine.performDepthCharge(initialState, lastIterationVisitedNodes);
+				succeededIterations++;
+				visitedNodes += lastIterationVisitedNodes[0];
+			} catch (TransitionDefinitionException | MoveDefinitionException e) {
+				GamerLogger.logError("SMSpeedTest", "Exception during iteration!");
+				GamerLogger.logStackTrace("SMSpeedTest", e);
+				failedIterations++;
+			}
 		}
 
-		long stop = System.currentTimeMillis();
+		exactTimeSpent = System.currentTimeMillis() - startTime;
 
-		GamerLogger.log("Stats", "VISITED_NODES = " + visitedNodes);
-		GamerLogger.log("Stats", "ITERATIONS = " + iterations);
-		GamerLogger.log("Stats", "MOVE_SELECTION_TIME = " + (stop - start));
-
-		notifyObservers(new GamerSelectedMoveEvent(moves, selection, stop - start));
-		return selection;
 	}
+
 }
