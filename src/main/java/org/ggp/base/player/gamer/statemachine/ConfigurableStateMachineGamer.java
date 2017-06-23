dfc15ba29df93a67a6d0da4dcf85569221597d74
Chiara <sironi.chiara.f@gmail.com>
2017-06-23 18:49:30 +0200

    Added some gamers settings.

--- src/main/java/org/ggp/base/player/gamer/statemachine/ConfigurableStateMachineGamer.java
+++ src/main/java/org/ggp/base/player/gamer/statemachine/ConfigurableStateMachineGamer.java
@@ -11,7 +11,7 @@ import org.ggp.base.util.logging.GamerLogger;
 
 public abstract class ConfigurableStateMachineGamer extends StateMachineGamer {
 
-	protected static final String defaultSettingsFileName = "RO50I25E1R05C03MMultiPopCondAREvoTunerCGMD.properties";
+	protected static final String defaultSettingsFileName = "RO50I25E100R05CSRMMultiPopCondAREvoTunerCGMD.properties";
 
 	protected String settingsFilePath;
 
